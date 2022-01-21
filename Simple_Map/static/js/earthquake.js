4a284bc09354d5f4b892a754e6dbec90f17da0a6
Ryan Morin <ryanjmorin@gmail.com>
2022-01-20 19:24:39 -0800

    earthquake

--- Simple_Map/static/js/earthquake.js
+++ Simple_Map/static/js/earthquake.js
@@ -1,9 +1,53 @@
-let map = L.map('mapid', {
-    center: [
-        40.7, -94.5
-    ],
-    zoom: 4
+console.log('working');
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+
+    /*
+    additional views: 
+    mapbox/outdoors-v11,
+    mapbox/light-v10,
+    mapbox/dark-v10,
+    mapbox/satellite-v9,
+    mapbox/satellite-streets-v11
+    */
+
+    accessToken: API_KEY
 });
+
+// We create the second tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let map = L.map('mapid', {
+    center: [40.7, -94.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Create a base layer that holds all three maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+};
+
+// 1. Add a 3rd layer group for the major earthquake data.
+let allEarthquakes = new L.LayerGroup();
+
+// 2. Add a reference to the major earthquake group to the overlays object.
+let overlays = {
+    "Earthquakes": allEarthquakes,
+
+};
+
+// Then we add a control to the map that will allow the user to change which
+// layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
@@ -16,7 +60,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: "#ffae42", //light orange
+            fillColor: getColor(feature.properties.mag),
             color: "#000000", //black
             radius: getRadius(feature.properties.mag),
             stroke: true,
@@ -33,32 +77,75 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return magnitude * 4;
     };
 
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    };
+
+    // Creating a GeoJSON layer with the retrieved data.
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
+        // We turn each feature into a circleMarker on the map.
         pointToLayer: function (feature, latlng) {
             console.log(data);
             return L.circleMarker(latlng);
         },
-        style: styleInfo
-    }).addTo(map);
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(allEarthquakes);
+
+    // Then we add the earthquake layer to our map.
+    allEarthquakes.addTo(map);
+
 });
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+  });
 
-    /*
-    additional views: 
-    mapbox/outdoors-v11,
-    mapbox/light-v10,
-    mapbox/dark-v10,
-    mapbox/satellite-v9,
-    mapbox/satellite-streets-v11
-    */
+// Then add all the details for the legend
+legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
 
-    accessToken: API_KEY
-});
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+    ];
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+};
+
+// Finally, we our legend to the map.
+legend.addTo(map);
