0cef65c49c8149b1b50b9346c71c32d5b0a277ca
Ryan Morin <ryanjmorin@gmail.com>
2022-01-20 18:01:48 -0800

    eatherquake 2

--- Simple_Map/static/js/earthquake.js
+++ Simple_Map/static/js/earthquake.js
@@ -7,8 +7,40 @@ let map = L.map('mapid', {
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into a function
+    // to calculate the radius.
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42", //light orange
+            color: "#000000", //black
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    };
+
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+    };
+
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map)
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo
+    }).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
